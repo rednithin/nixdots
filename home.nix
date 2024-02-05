a34d61fade113b574f1cb5ef5f101f26830f576c
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-02-06 01:27:23 +0530

    xautolock

--- home.nix
+++ home.nix
@@ -62,7 +62,7 @@
     zoxide
     hyperfine
     xorg.xprop
-
+    xautolock
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
