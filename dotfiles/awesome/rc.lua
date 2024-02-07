446085781b281a8958576382aef39f9933a62456
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-02-07 21:20:56 +0530

    height

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -212,7 +212,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 30 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
