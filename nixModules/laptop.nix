0ec7856c66240453a2223103272b96c8632c2179
Nithin <reddy.nithinpg@gmail.com>
2024-04-10 21:33:10 +0530

    laptop stuff

--- nixModules/laptop.nix
+++ nixModules/laptop.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }: 
 {
     boot.kernelPackages = pkgs.linuxPackages_6_8;
+
+    services.thermald.enable = true;
+
+    services.auto-cpufreq.enable = true;
+    services.auto-cpufreq.settings = {
+        battery = {
+            governor = "powersave";
+            turbo = "never";
+        };
+        charger = {
+            governor = "performance";
+            turbo = "auto";
+        };
+    };
+
+    # powerManagement.powertop.enable = true;
 }
