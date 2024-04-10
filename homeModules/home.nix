94f38135c16c7e1b9969662e79fcf614587ad91d
Nithin <reddy.nithinpg@gmail.com>
2024-04-10 21:28:56 +0530

    add brightnessctl

--- homeModules/home.nix
+++ homeModules/home.nix
@@ -70,6 +70,7 @@
     insomnia
     gparted
     isoimagewriter
+    brightnessctl
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
