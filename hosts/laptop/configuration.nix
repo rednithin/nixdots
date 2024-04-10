20f457fcb9c02b59e56b4db7345a47173752680a
Nithin <reddy.nithinpg@gmail.com>
2024-04-10 12:19:28 +0530

    added

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../nixModules/nixosconfig.nix
     ];
 
   # Bootloader.
@@ -96,11 +97,11 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-	brightnessctl
-	neofetch
-       screenfetch
-	git
-	vscode
+	  brightnessctl
+	  neofetch
+    screenfetch
+	  git
+	  vscode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
