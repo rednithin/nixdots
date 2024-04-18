2eee60ce8286c83417235cd6d74afff07811a90b
Nithin <reddy.nithinpg@gmail.com>
2024-04-19 00:45:28 +0530

    some stuff

--- nixModules/laptop.nix
+++ nixModules/laptop.nix
@@ -4,16 +4,20 @@
 
     services.thermald.enable = true;
 
-    services.auto-cpufreq.enable = true;
-    services.auto-cpufreq.settings = {
-        battery = {
-            governor = "powersave";
-            turbo = "never";
-        };
-        charger = {
-            governor = "performance";
-            turbo = "auto";
-        };
+    # services.auto-cpufreq.enable = true;
+    # services.auto-cpufreq.settings = {
+    #     battery = {
+    #         governor = "powersave";
+    #         turbo = "never";
+    #     };
+    #     charger = {
+    #         governor = "performance";
+    #         turbo = "auto";
+    #     };
+    # };
+
+    services.tlp = {
+        enable = true;
     };
 
     # powerManagement.powertop.enable = true;
