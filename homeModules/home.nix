77b81b27676874e26a3fe06faabe75fa38c38011
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-05-18 16:52:09 +0530

    sqlite

--- homeModules/home.nix
+++ homeModules/home.nix
@@ -75,6 +75,8 @@
     iperf
     mongodb-compass
     mongodb
+    sqlite
+    sqlitebrowser
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
