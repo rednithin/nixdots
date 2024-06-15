d52423d5848135640897016d41f75daac9944865
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-06-15 17:49:52 +0530

    some stuff

--- homeModules/home.nix
+++ homeModules/home.nix
@@ -94,6 +94,7 @@
     wofi
     pavucontrol
     
+    alacritty
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
