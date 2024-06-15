74394eb8b3b575645ccb2a526dc254a0e46a68ba
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-06-15 23:53:21 +0530

    some change

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -50,7 +50,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
