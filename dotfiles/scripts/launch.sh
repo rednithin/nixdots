2c1213e80305835bc38482c4883907b44eeb9202
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-06-16 13:43:25 +0530

    add dolphin

--- dotfiles/scripts/launch.sh
+++ dotfiles/scripts/launch.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 
-sleep 3
+sleep 1
 
 hyprctl dispatch exec [workspace 1 silent] code
-sleep 2
+hyprctl dispatch exec [workspace 2 silent] dolphin
 hyprctl dispatch exec [workspace 3 silent] alacritty
-sleep 2
 hyprctl dispatch exec [workspace 5 silent] firefox
-sleep 2
 hyprctl dispatch exec [workspace 10 silent] spotify
