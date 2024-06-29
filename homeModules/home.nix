fa03f5ba14033c6e33e75949a105239f52167ab7
Nithin <reddy.nithinpg@gmail.com>
2024-06-29 11:59:45 +0530

    added wlr randr

--- homeModules/home.nix
+++ homeModules/home.nix
@@ -102,6 +102,7 @@
     pamixer
     swaylock
     tailscale
+    wlr-randr
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
