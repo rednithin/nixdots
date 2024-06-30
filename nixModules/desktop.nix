469bc3791eba3ad0896a53d90d03c03926457f8d
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-06-30 14:18:59 +0530

    desktop different kernel

--- nixModules/desktop.nix
+++ nixModules/desktop.nix
@@ -2,7 +2,7 @@
 
 {
 
-  boot.kernelPackages = pkgs.linuxPackages_6_9;
+  boot.kernelPackages = pkgs.linuxPackages_6_8;
   
   fileSystems."/home/nithin/HDD" = { 
     device = "/dev/disk/by-uuid/9f66e623-f475-40ad-8a7c-97518b4f0656";
