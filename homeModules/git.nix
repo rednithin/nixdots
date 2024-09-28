d261eec10227781c1dc7d83fae1b0b06cd27dbaf
Nithin Reddy <reddy.nithinpg@gmail.com>
2024-09-28 08:15:47 +0000

    workaround for bad permissions error

--- homeModules/git.nix
+++ homeModules/git.nix
@@ -1,4 +1,11 @@
 { ... }: {
+
+  # Workaround for BadPermissions error
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
+
   programs.ssh = {
     enable = true;
     extraConfig = ''
