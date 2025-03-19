b3583d8d4e4bb659df19ec946fc949e47ba0ac72
Nithin <reddy.nithinpg@gmail.com>
2025-03-20 02:47:25 +0530

    update

--- homeModules/home.nix
+++ homeModules/home.nix
@@ -94,7 +94,7 @@
       sshpass
       starship
       swww
-      tailscale
+      # tailscale
       upower
       waybar
       wf-recorder
@@ -141,4 +141,23 @@
     ./zsh.nix
     ./terminal.nix
   ];
+
+
+  home.file.".local/share/icons/hicolor/16x16/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/16x16/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/24x24/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/24x24/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/32x32/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/32x32/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/48x48/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/48x48/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/64x64/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/64x64/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/128x128/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/128x128/apps/google-chrome.png";
+  home.file.".local/share/icons/hicolor/256x256/apps/google-chrome.png".source = "${pkgs.google-chrome}/share/icons/hicolor/256x256/apps/google-chrome.png";
+
+
+  home.file.".local/share/icons/hicolor/16x16/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/16x16/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/24x24/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/24x24/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/32x32/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/32x32/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/48x48/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/48x48/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/64x64/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/64x64/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/128x128/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/128x128/apps/firefox.png";
+  home.file.".local/share/icons/hicolor/256x256/apps/firefox.png".source = "${pkgs.firefox}/share/icons/hicolor/256x256/apps/firefox.png";
+
 }
--- homeModules/terminal.nix
+++ homeModules/terminal.nix
@@ -1,10 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.kitty = {
     enable = true;
     themeFile = "Catppuccin-Mocha";
     settings = {
       font_size = 14;
-      font_family = "FantasqueSansM Nerd Font Mono Regular";
+      font_family = "FantasqueSansM Nerd Font";
       enable_audio_bell = false;
       scrollback_lines = 100000;
       strip_trailing_spaces = "smart";
@@ -12,4 +12,26 @@
       background_blur = 1;
     };
   };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+      editor.softwrap = true;
+    };
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
+    themes = {
+    };
+  };
+
+
+
 }
